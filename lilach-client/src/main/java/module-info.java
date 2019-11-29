7a30d26ef59d27136e6741242703a84e12d27934
Liel Fridman <lielft@gmail.com>
2019-11-29 20:21:14 +0200

    Removing unnecessary line from client module-info.java

--- lilach-client/src/main/java/module-info.java
+++ lilach-client/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.cshaifasweng.winter {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.springframework.boot;
 
     opens org.cshaifasweng.winter to javafx.fxml;
     exports org.cshaifasweng.winter;
