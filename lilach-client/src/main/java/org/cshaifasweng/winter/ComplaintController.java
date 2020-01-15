e104cd9d820960c956005e8c8b1f036529a4e71a
Yaron <syarons1@gmail.com>
2020-01-15 21:02:32 +0200

    shit that doesn't work

---
+++ lilach-client/src/main/java/org/cshaifasweng/winter/ComplaintController.java
@@ -0,0 +1,53 @@
+package org.cshaifasweng.winter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ComplaintController implements Refreshable {
+
+    @FXML
+    private Label complaintLable;
+
+    @FXML
+    private TextArea complaintBox;
+
+    @FXML
+    private CheckBox emailCheckBox;
+
+    @FXML
+    private Button sendBox;
+
+    @FXML
+    private Button cancleBox;
+
+    @FXML
+    private CheckBox pruchasedCheckBox;
+
+    @FXML
+    private Button clearcomplaintbox;
+
+    @FXML
+    private Label orderNumberLable;
+
+    @FXML
+    private TextField orderNumberFild;
+
+    @FXML
+    void cancelComplaint(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enableOrderNumber(MouseEvent event) {
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+}
