e849ff215111f1144db825ff9e952d27455844ea
Yaron <syarons1@gmail.com>
2020-01-19 16:10:52 +0200

    Added input checks.

--- lilach-client/src/main/java/org/cshaifasweng/winter/ComplaintController.java
+++ lilach-client/src/main/java/org/cshaifasweng/winter/ComplaintController.java
@@ -1,5 +1,6 @@
 package org.cshaifasweng.winter;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.cshaifasweng.winter.events.DashboardSwitchEvent;
+import org.greenrobot.eventbus.EventBus;
 
 public class ComplaintController implements Refreshable {
 
@@ -37,11 +40,26 @@ public class ComplaintController implements Refreshable {
     @FXML
     private TextField orderNumberFild;
 
+    @FXML
+    private Label orderNumEmpty;
+
+    @FXML
+    private Label compEmpty;
+
+    @FXML
+    private Label invalidInputOrNum;
+
 
     private boolean status = true;
 
     @FXML
     void cancelComplaint(MouseEvent event) {
+        EventBus.getDefault().post(new DashboardSwitchEvent("primary"));
+    }
+
+    @FXML
+    void clearText(ActionEvent event) {
+        complaintBox.setText("");
 
     }
 
@@ -50,6 +68,53 @@ public class ComplaintController implements Refreshable {
         status = !status;
         orderNumberLable.setDisable(status);
         orderNumberFild.setDisable(status);
+    }
+
+    private boolean isValid (TextField fieldA){
+        int countDot = 0;
+        for (int i = 0; i<fieldA.getText().length() ; i++){
+            char temp = fieldA.getText().charAt(i);
+            if(!(temp >= '0') && (temp <= '9')){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean inputCheck (){
+        boolean retVal = true;
+
+        if (complaintBox.getText().isEmpty())
+        {
+            compEmpty.setVisible(true);
+            retVal = false;
+        }
+        if (pruchasedCheckBox.isSelected())
+        {
+            if (orderNumberFild.getText().isEmpty())
+            {
+                orderNumEmpty.setVisible(true);
+                retVal = false;
+            }
+        }
+        if (!(isValid(orderNumberFild)))
+        {
+            invalidInputOrNum.setVisible(true);
+            retVal = false;
+        }
+        return retVal;
+    }
+
+    private void restVisible()
+    {
+        orderNumEmpty.setVisible(false);
+        compEmpty.setVisible(false);
+    }
+
+    @FXML
+    void sendComplaint(ActionEvent event) {
+        restVisible();
+        inputCheck();
     }
 
     @Override
--- lilach-client/src/main/java/org/cshaifasweng/winter/HandleComplaintController.java
+++ lilach-client/src/main/java/org/cshaifasweng/winter/HandleComplaintController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.cshaifasweng.winter.events.DashboardSwitchEvent;
+import org.greenrobot.eventbus.EventBus;
+
+import javax.swing.*;
 
 public class HandleComplaintController implements Refreshable {
 
@@ -32,25 +36,75 @@ public class HandleComplaintController implements Refreshable {
     @FXML
     private Label empty;
 
+    @FXML
+    private Label compensationEmpty;
+
+    @FXML
+    private Label invalidInput;
 
     private boolean status = true;
 
     @FXML
     void cancel(ActionEvent event) {
-
+        EventBus.getDefault().post(new DashboardSwitchEvent("primary"));
     }
 
     @FXML
     void clearText(ActionEvent event) {
         answerBox.setText("");
     }
+    private boolean isValid (TextField fieldA){
+        int countDot = 0;
+        for (int i = 0; i<fieldA.getText().length() ; i++){
+            char temp = fieldA.getText().charAt(i);
+            if (temp == '.') {
+                countDot++;
+                if (countDot > 1){
+                    return false;
+                }
+            }
+            if(!((temp>='0' && temp<='9') || (temp == '.'))){
+                return false;
+            }
+        }
+        return true;
+    }
 
-    @FXML
-    void sendFrom(ActionEvent event) {
+    private boolean inputCheck (){
+        boolean retVal = true;
         if (answerBox.getText().isEmpty())
         {
             empty.setVisible(true);
+            retVal = false;
         }
+        if (finComp.isSelected())
+        {
+            if (compAmountFild.getText().isEmpty())
+            {
+                compensationEmpty.setVisible(true);
+                retVal = false;
+            }
+        }
+        if (!(isValid(compAmountFild)))
+        {
+            invalidInput.setVisible(true);
+            retVal = false;
+        }
+        return retVal;
+    }
+
+    @FXML
+    void sendFrom(ActionEvent event) {
+       restVisible();
+       inputCheck();
+
+    }
+
+    private void restVisible()
+    {
+        empty.setVisible(false);
+        compensationEmpty.setVisible(false);
+        invalidInput.setVisible(false);
     }
 
     @FXML
