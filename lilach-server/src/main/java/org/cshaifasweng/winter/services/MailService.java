afd993f45e6d332f87e439067897895e829ba17b
Liel Fridman <lielft@gmail.com>
2020-01-22 11:46:13 +0200

    Safeguard the mail service so it does not send emails to fake addresses

--- lilach-server/src/main/java/org/cshaifasweng/winter/services/MailService.java
+++ lilach-server/src/main/java/org/cshaifasweng/winter/services/MailService.java
@@ -1,6 +1,8 @@
 package org.cshaifasweng.winter.services;
 
 import org.cshaifasweng.winter.SpringServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -19,6 +21,8 @@ public class MailService {
     @Value("${lilach.mail.from}")
     private String mailFrom;
 
+    private static final Logger log = LoggerFactory.getLogger(MailService.class);
+
     @Autowired
     public MailService(JavaMailSender mailSender, MailContentBuilder contentBuilder) {
         this.mailSender = mailSender;
@@ -26,10 +30,15 @@ public class MailService {
     }
 
     public void sendMail(String recipient, String subject, String message) {
+        if (recipient.endsWith("@lilach.com")) {
+            log.warn("Tried to send email to address in the lilach.com domain, which is prohibited. Will self-send instead.");
+            recipient = mailFrom;
+        }
+        String finalRecipient = recipient;
         MimeMessagePreparator preparator = mimeMessage -> {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
             helper.setFrom(mailFrom);
-            helper.setTo(recipient);
+            helper.setTo(finalRecipient);
             helper.setSubject(subject);
             helper.setText(contentBuilder.build(subject, message), true);
             helper.addInline("lilach-logo",
