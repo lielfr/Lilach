a2f76b404905b059fe81afa4c6a2eed7de35eb77
Liel Fridman <lielft@gmail.com>
2020-01-22 19:37:02 +0200

    Syncing entities

--- lilach-client/src/main/java/org/cshaifasweng/winter/models/Employee.java
+++ lilach-client/src/main/java/org/cshaifasweng/winter/models/Employee.java
@@ -12,6 +12,14 @@ public class Employee extends User {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date employedSince;
 
+    private Store managedStore;
+
+    private Store assignedStore;
+
+    private Collection<Employee> subordinates;
+
+    private Employee reportsTo;
+
     private Collection<Complaint> handledComplaints;
 
     public Employee() {}
@@ -19,6 +27,13 @@ public class Employee extends User {
     @Override
     public User copy() {
         Employee copy = new Employee(email, password, firstName, lastName, phone, (Date) employedSince.clone());
+        if (managedStore != null)
+            copy.setManagedStore(new Store(managedStore));
+        if (reportsTo != null)
+            copy.setReportsTo((Employee) reportsTo.copy());
+        copy.setHandledComplaints(handledComplaints);
+        if (assignedStore != null)
+            copy.setAssignedStore(new Store(assignedStore));
         return copy;
     }
 
@@ -42,5 +57,37 @@ public class Employee extends User {
 
     public void setHandledComplaints(Collection<Complaint> handledComplaints) {
         this.handledComplaints = handledComplaints;
+    }
+
+    public Store getManagedStore() {
+        return managedStore;
+    }
+
+    public void setManagedStore(Store managedStore) {
+        this.managedStore = managedStore;
+    }
+
+    public Collection<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(Collection<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
+
+    public Employee getReportsTo() {
+        return reportsTo;
+    }
+
+    public void setReportsTo(Employee reportsTo) {
+        this.reportsTo = reportsTo;
+    }
+
+    public Store getAssignedStore() {
+        return assignedStore;
+    }
+
+    public void setAssignedStore(Store assignedStore) {
+        this.assignedStore = assignedStore;
     }
 }
