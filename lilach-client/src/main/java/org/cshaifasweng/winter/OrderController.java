f4023ecf30248b3a64e75fa47ede2e6be9d2e0b9
Yaron <syarons1@gmail.com>
2020-01-25 00:14:57 +0200

    order screen added fully.

--- lilach-client/src/main/java/org/cshaifasweng/winter/OrderController.java
+++ lilach-client/src/main/java/org/cshaifasweng/winter/OrderController.java
@@ -1,41 +1,164 @@
 package org.cshaifasweng.winter;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import org.cshaifasweng.winter.events.DashboardSwitchEvent;
-import org.greenrobot.eventbus.EventBus;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-import java.io.IOException;
+public class OrderController {
 
+    @FXML
+    private Button backButton;
 
-public class OrderController implements Refreshable {
+    @FXML
+    private Button cancelButton;
 
+    @FXML
+    private Button nextButton;
 
-    @Override
-    public void refresh() {
+    @FXML
+    private Tab tab1;
+
+    @FXML
+    private TableView<?> itemTable;
+
+    @FXML
+    private Tab tab2;
+
+    @FXML
+    private TextField firstNameVerField;
+
+    @FXML
+    private TextField lastNameVerField;
+
+    @FXML
+    private TextField idNumVerField;
+
+    @FXML
+    private TextField emailVerField;
+
+    @FXML
+    private TextField phoneNumVerField;
+
+    @FXML
+    private TextField addressVerField;
+
+    @FXML
+    private Label firstNameVerEmptyLabel;
+
+    @FXML
+    private Label lastNameVerEmptyLabel;
+
+    @FXML
+    private Label idVerEmptyLabel;
+
+    @FXML
+    private Label emailVerEmptyLabel;
+
+    @FXML
+    private Label phoneVerEmptyLabel;
+
+    @FXML
+    private Label addressVerEmptyLabel;
+
+    @FXML
+    private Label invalidFirstNameVerLabel;
+
+    @FXML
+    private Label invalidLastNameVerLabel;
+
+    @FXML
+    private Label invalidIdVerLabel;
+
+    @FXML
+    private Label invalidEmailVerLabel;
+
+    @FXML
+    private Label invalidPhoneVerLabel;
+
+    @FXML
+    private Label invalidAddressVerLabel;
+
+    @FXML
+    private Button changeDetailsButton;
+
+    @FXML
+    private Tab tab3;
+
+    @FXML
+    private TextArea greetingEntryTextArea;
+
+    @FXML
+    private Tab tab4;
+
+    @FXML
+    private RadioButton sendToMyAddRadio;
+
+    @FXML
+    private RadioButton sendToAnotherAddRadio;
+
+    @FXML
+    private TextField deliveryAddressField;
+
+    @FXML
+    private TextField recipientMailField;
+
+    @FXML
+    private Label deliveryAddressEmpty;
+
+    @FXML
+    private Label recipientMailEmpty;
+
+    @FXML
+    private Label invalidEmailAddressLabel;
+
+    @FXML
+    private Tab tab5;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField idNumField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextArea greetingTextArea;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
     }
 
     @FXML
-    private void goToPayment() throws IOException {
-        EventBus.getDefault().post(new DashboardSwitchEvent("order_payment"));
+    void changeDetails(ActionEvent event) {
+
     }
 
     @FXML
-    private void goToOrderCompleted() throws IOException {
-        EventBus.getDefault().post(new DashboardSwitchEvent("order_completed"));
+    void goBack(ActionEvent event) {
+
     }
 
     @FXML
-    private void backToEditOrder() throws IOException {
-        EventBus.getDefault().post(new DashboardSwitchEvent("order"));
+    void next(ActionEvent event) {
+
     }
-
-    @FXML
-    private void backToCart() throws IOException {
-        EventBus.getDefault().post(new DashboardSwitchEvent("Customer_Cart"));
-    }
-
-
-
-
 
 }
