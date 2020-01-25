636f124d9ed279ba20b29491fc88f716dadc1cb8
Liel Fridman <lielft@gmail.com>
2020-01-25 22:08:41 +0200

    Began expirementing with uploads

--- lilach-server/src/main/java/org/cshaifasweng/winter/api/ImageController.java
+++ lilach-server/src/main/java/org/cshaifasweng/winter/api/ImageController.java
@@ -1,13 +1,29 @@
 package org.cshaifasweng.winter.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.cshaifasweng.winter.SpringServer;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.UUID;
 
 @RestController
 public class ImageController {
     @GetMapping("/image/{id}")
-    public byte[] getImage(@PathVariable String id) {
-        return null;
+    public byte[] getImage(@PathVariable("id") String id) throws IOException {
+        File file = new File(String.valueOf(SpringServer.class.getResource("uploads/" + id)));
+        return new FileInputStream(file).readAllBytes();
+    }
+
+    @PostMapping("/image")
+    public String uploadImage(@RequestBody MultipartFile file) throws IOException {
+        String uuid = UUID.randomUUID() + "." + file.getOriginalFilename().split(".")[1];
+        FileOutputStream outputStream =
+                new FileOutputStream(
+                        new File(
+                                String.valueOf(SpringServer.class.getResource("uploads")) + "/" + uuid));
+        outputStream.write(file.getBytes());
+        outputStream.close();
+        return uuid;
     }
 }
