6dacac61d8afc35ec0bfa4f0df218a9c65a76134
Liel Fridman <lielft@gmail.com>
2020-01-25 21:57:15 +0200

    Changing return type to byte[]

--- lilach-server/src/main/java/org/cshaifasweng/winter/api/ImageController.java
+++ lilach-server/src/main/java/org/cshaifasweng/winter/api/ImageController.java
@@ -1,6 +1,5 @@
 package org.cshaifasweng.winter.api;
 
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ImageController {
     @GetMapping("/image/{id}")
-    public Resource getImage(@PathVariable String id) {
+    public byte[] getImage(@PathVariable String id) {
         return null;
     }
 }
