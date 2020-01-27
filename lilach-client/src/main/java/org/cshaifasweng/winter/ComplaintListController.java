8e78828fe739d7f1ba1b259145108ad3a01f5ea0
Yaron <syarons1@gmail.com>
2020-01-27 17:14:56 +0200

    complaint list screen and controller created.

---
+++ lilach-client/src/main/java/org/cshaifasweng/winter/ComplaintListController.java
@@ -0,0 +1,21 @@
+package org.cshaifasweng.winter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class ComplaintListController {
+
+    @FXML
+    private TableView<?> complaintListTable;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void back(MouseEvent event) {
+
+    }
+
+}
