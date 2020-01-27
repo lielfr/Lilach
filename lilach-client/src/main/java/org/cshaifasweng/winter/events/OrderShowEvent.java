ab6d08632a0d36cf8c604d3f5333652f344b8e81
Yaron <syarons1@gmail.com>
2020-01-27 22:19:23 +0200

    order show event implemented.

--- lilach-client/src/main/java/org/cshaifasweng/winter/events/OrderShowEvent.java
+++ lilach-client/src/main/java/org/cshaifasweng/winter/events/OrderShowEvent.java
@@ -1,4 +1,15 @@
 package org.cshaifasweng.winter.events;
 
+import org.cshaifasweng.winter.models.Order;
+
 public class OrderShowEvent {
+    private Order order;
+
+    public OrderShowEvent(Order order){
+        this.order = order;
+    }
+
+    public Order getOrder(){
+        return order;
+    }
 }
