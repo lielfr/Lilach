f2ab5c48ea7506bcc32a63213f6e1fcb1e448ac9
Liel Fridman <lielft@gmail.com>
2020-01-28 17:56:22 +0200

    Added parameters to CustomerSendEvent

--- lilach-client/src/main/java/org/cshaifasweng/winter/events/CustomerSendEvent.java
+++ lilach-client/src/main/java/org/cshaifasweng/winter/events/CustomerSendEvent.java
@@ -1,15 +1,22 @@
 package org.cshaifasweng.winter.events;
 
+import javafx.stage.Stage;
 import org.cshaifasweng.winter.models.Customer;
 
 public class CustomerSendEvent {
     private Customer customer;
+    private Stage stage;
 
-    public CustomerSendEvent(Customer customer) {
+    public CustomerSendEvent(Customer customer, Stage stage) {
         this.customer = customer;
+        this.stage = stage;
     }
 
     public Customer getCustomer() {
         return customer;
+    }
+
+    public Stage getStage() {
+        return stage;
     }
 }
