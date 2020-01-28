ec0a7964a813d2b2a21999620a7f9101f2450859
Yaron <syarons1@gmail.com>
2020-01-28 16:04:42 +0200

    Adding customer edit

---
+++ lilach-client/src/main/java/org/cshaifasweng/winter/EditCustomerController.java
@@ -0,0 +1,115 @@
+package org.cshaifasweng.winter;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class EditCustomerController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField firsNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField idNumField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField creditcardField;
+
+    @FXML
+    private TextField cvvField;
+
+    @FXML
+    private DatePicker dateOfBirthPicker;
+
+    @FXML
+    private DatePicker expirationPicker;
+
+    @FXML
+    private DatePicker subscriptionEndPicker;
+
+    @FXML
+    private ChoiceBox<?> subscriptionChoice;
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Label idNumLabel;
+
+    @FXML
+    private Label phoneNumLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label dateOfBirthLabel;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private Label addressLabel;
+
+    @FXML
+    private Label creditcardLabel;
+
+    @FXML
+    private Label cvvLabel;
+
+    @FXML
+    private Label expirationLabel;
+
+    @FXML
+    private Label subscriptionEndLabel;
+
+    @FXML
+    private Label customerIdLabel;
+
+    @FXML
+    void edit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveChanges(ActionEvent event) {
+
+    }
+
+}
